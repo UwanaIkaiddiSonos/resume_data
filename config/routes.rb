6d00b224053bcc7b001f88ac005d2ec31ec38372
Uwana <uwikaiddi@gmail.com>
2019-04-18 18:48:37 -0500

    Add paths to routs file

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  # EXAMPLE HTML ROUTE
-  # get "/photos" => "photos#index"
 
-  # EXAMPLE JSON ROUTE WITH API NAMESPACE
-  # namespace :api do
-  #   get "/photos" => "photos#index"
-  # end
+  namespace :api do
+    get "/students" => "students#index"
+    post "/students" => "students#create"
+    get "/students" => "students#show"
+    patch "/students" => "students#update"
+    delete "/students" => "students#destroy"    
+  end
 end
