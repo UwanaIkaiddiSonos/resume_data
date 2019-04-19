90f0656f8c1fbbfdf2ede8bf798f7add1928ccea
Uwana <uwikaiddi@gmail.com>
2019-04-18 21:06:03 -0500

    Add create method

--- app/controllers/api/students_controller.rb
+++ app/controllers/api/students_controller.rb
@@ -11,7 +11,7 @@ class Api::StudentsController < ApplicationController
 
   def create
     @student = Student.new(
-    first_name: params[:first_name],
+    first_name: params[:first_name] || @student.first_name,
     last_name: params[:last_name],
     email: params[:email],
     phone_number: params[:phone_number],
@@ -28,16 +28,24 @@ class Api::StudentsController < ApplicationController
     render 'show.json.jbuilder'
   end
 
-  # def update
-  #   @student = Student.find_by(id: params[:id])
+  def update
+    @student = Student.find_by(id: params[:id])
 
-  #   @student.update(
-  #     first_name: params[:input_first_name] || @student.first_name,
-  #     last_name: params[:input_last_name] || @student.last_name,
-  #     color: params[:input_color] || @student.color
-  #   )
-  #   render 'show.json.jbuilder'
-  # end
+    @student.update(
+      first_name: params[:first_name] || @student.first_name,
+      last_name: params[:last_name] || @student.last_name,
+      email: params[:email] || @student.email,
+      phone_number: params[:phone_number] || @student.phone_number,
+      short_bio: params[:short_bio] || @student.short_bio,
+      linkedin: params[:linkedin] || @student.linkedin,
+      twitter: params[:twitter] || @student.twitter,
+      website: params[:website] || @student.website,
+      resume: params[:resume] || @student.resume,
+      github: params[:github] || @student.github,
+      photo: params[:photo] || @student.photo
+    )
+    render 'show.json.jbuilder'
+  end
 
   # def destroy
   #   @student = Student.find_by(id: params[:id])
