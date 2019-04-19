5da4be44b415bbc47b50d5965d9bfe0a861ec0f6
Uwana <uwikaiddi@gmail.com>
2019-04-18 20:41:21 -0500

    Add create route and method

--- app/controllers/api/students_controller.rb
+++ app/controllers/api/students_controller.rb
@@ -11,29 +11,37 @@ class Api::StudentsController < ApplicationController
 
   def create
     @student = Student.new(
-    first_name: params[:input_first_name],
-    last_name: params[:input_last_name],
-    color: params[:input_color]
+    first_name: params[:first_name],
+    last_name: params[:last_name],
+    email: params[:email],
+    phone_number: params[:phone_number],
+    short_bio: params[:short_bio],
+    linkedin: params[:linkedin],
+    twitter: params[:twitter],
+    website: params[:website],
+    resume: params[:resume],
+    github: params[:github],
+    photo: params[:photo]
     )
 
     @student.save
     render 'show.json.jbuilder'
   end
 
-  def update
-    @student = Student.find_by(id: params[:id])
+  # def update
+  #   @student = Student.find_by(id: params[:id])
 
-    @student.update(
-      first_name: params[:input_first_name] || @student.first_name,
-      last_name: params[:input_last_name] || @student.last_name,
-      color: params[:input_color] || @student.color
-    )
-    render 'show.json.jbuilder'
-  end
+  #   @student.update(
+  #     first_name: params[:input_first_name] || @student.first_name,
+  #     last_name: params[:input_last_name] || @student.last_name,
+  #     color: params[:input_color] || @student.color
+  #   )
+  #   render 'show.json.jbuilder'
+  # end
 
-  def destroy
-    @student = Student.find_by(id: params[:id])
-    @student.destroy
-    render "destroy.json.jbuilder"
-  end
+  # def destroy
+  #   @student = Student.find_by(id: params[:id])
+  #   @student.destroy
+  #   render "destroy.json.jbuilder"
+  # end
 end
