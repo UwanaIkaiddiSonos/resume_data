99bf12596215c0dbc155970dbf513f450536a143
Susmita Bhowmik <susmita.bhowmik516@gmail.com>
2019-04-18 22:07:13 -0400

    update index and show to include partial

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_18_234928) do 
+ActiveRecord::Schema.define(version: 2019_04_18_234928) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,9 @@ ActiveRecord::Schema.define(version: 2019_04_18_234928) do
     t.string "description"
     t.string "url"
     t.string "screenshot"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "experiences", force: :cascade do |t|
     t.string "start_date"
@@ -46,5 +49,5 @@ ActiveRecord::Schema.define(version: 2019_04_18_234928) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-  
+
 end
