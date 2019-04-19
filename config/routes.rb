a5c044486f5ef9a9525701485d902065645aae3d
Adrian <a.fernandez1@live.com>
2019-04-18 18:20:29 -0700

    education section

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  # EXAMPLE HTML ROUTE
-  # get "/photos" => "photos#index"
-
-  # EXAMPLE JSON ROUTE WITH API NAMESPACE
-  # namespace :api do
-  #   get "/photos" => "photos#index"
-  # end
+  namespace :api do
+    get '/educations' => 'educations#index'
+    get '/educations/:id' => 'educations#show'
+    post '/educations' => 'educations#create'
+    patch '/educations/:id' => 'educations#update'
+    delete '/educations/:id' => 'educations#destroy'
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_17_013848) do
+ActiveRecord::Schema.define(version: 2019_04_19_000649) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "educations", force: :cascade do |t|
+    t.string "start_date"
+    t.string "end_date"
+    t.string "degree"
+    t.string "university_name"
+    t.text "details"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "students", force: :cascade do |t|
     t.string "first_name"
