8d1486073d44f7618cd7e0eff6e9851be89563b9
Adrian <a.fernandez1@live.com>
2019-04-22 17:42:24 -0700

    added the following models

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,14 @@
 Education.create({start_date: "November 2012", end_date: "June 2018", degree: "Bachelors of Arts Trigonometry", university_name: "University of Trigonometry", details: "studied math and that of the such"})
+
+Experience.create({ start_date: "2/10/2012", end_date: "3/15/2014", job_title: "Singer", company_name: "American Idol", details: "Produced 10 albums and won 15 Grammys"})
+
+Experience.create({start_date: "6/01/2015", end_date: "9/01/2015", job_title: "Astronaut", company_name: "NASA", details: "Travelled to the moon and then went to Mars"})
+
+Experience.create({start_date: "3/02/2012", end_date: "6/17/2014", job_title: "Software Developer", company_name: "Amazon", details: "Worked within a team of 100 people to maintain and update Amazon's web interface"})
+
+Skill.create({skill_name: "CSS"})
+Skill.create({skill_name: "HTML"})
+Skill.create({skill_name: "Bootstrap"})
+Skill.create({skill_name: "JavaScript"})
+Skill.create({skill_name: "Ruby"})
+Skill.create({skill_name: "Ruby on Rails"})
